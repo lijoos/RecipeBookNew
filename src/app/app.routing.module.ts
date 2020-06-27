bd496460175b84afd03f5c3e359ea936c8ba9b42
lijo <lijo.joseph@allianz.co.id>
2020-06-27 16:02:18 +0700

    Fix:

---
+++ src/app/app.routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule, Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { Routes } from '@angular/router';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
+
+const appRoutes: Routes = [
+{path: '' , redirectTo: '/recipes', pathMatch: 'full'},
+{path : 'shopping-list' , component: ShoppingListComponent},
+{path: 'recipes' , component: RecipesComponent , 
+  children: [
+       {path: '' , component : RecipeStartComponent},
+       {path: 'new' , component : RecipeEditComponent},
+       {path: ':id' , component : RecipeDetailComponent},
+       {path: ':id/edit' , component : RecipeEditComponent}
+]}
+];
+@NgModule({
+  imports: [RouterModule.forRoot(appRoutes)],
+   exports: [RouterModule]
+})
+export class AppRoutingModule {
+
+}
---
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-recipe-detail',
+  templateUrl: './recipe-detail.component.html',
+  styleUrls: ['./recipe-detail.component.css']
+})
+export class RecipeDetailComponent implements OnInit {
+  recipe: Recipe;
+  id: number;
+  constructor(private recipeService: RecipeService, private route: ActivatedRoute, private router: Router) { }
+
+  ngOnInit() {
+  this.route.params.subscribe((param: Params) => {
+    this.id = +param['id'];
+    this.recipe = this.recipeService.getRecipe(this.id);
+  });
+
+  }
+
+  onAddToShoppingList() {
+    this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
+  }
+  OnRecipeEdit() {
+   this.router.navigate(['edit'], {relativeTo: this.route});
+  }
+}
